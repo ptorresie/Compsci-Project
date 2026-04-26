b44883fa1da73706d4f29b97f1ddd3014f3bbff0
Pablo Garma Tardío <pgt01@MacBook-Air-de-Pablo.local>
2026-04-26 10:35:28 +0200

    stage4: fixed

--- src/stage2_euler/main_v2.py
+++
@@ -1,6 +0,0 @@
-from compute_euler import compute_euler
-from plot_euler import plot
-
-data = compute_euler()
-chosen_n = int(input("Enter which n to plot: "))
-plot(data, chosen_n)
--- src/stage3_euler_improved/main_v3.py
+++
@@ -1,7 +0,0 @@
-from compute_euler_improved import compute_euler_improved
-from plot_euler_improved import plot
-
-data = compute_euler_improved()
-print(data)
-chosen_n = int(input("Enter which n to plot: "))
-plot(data, chosen_n)
